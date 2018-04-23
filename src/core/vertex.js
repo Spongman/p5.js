b6db96289ab6a9c0ba10e629dd758d532561f34f
piersh <piersh@hotmail.com>
2018-04-23 09:36:51 -0700

    add validation to more methods

--- src/core/vertex.js
+++ src/core/vertex.js
@@ -62,6 +62,7 @@ var isFirstContour = true;
  *
  */
 p5.prototype.beginContour = function() {
+  p5._validateParameters('beginContour', arguments);
   contourVertices = [];
   isContour = true;
   return this;
@@ -447,6 +448,7 @@ p5.prototype.curveVertex = function(x, y) {
  *
  */
 p5.prototype.endContour = function() {
+  p5._validateParameters('endContour', arguments);
   var vert = contourVertices[0].slice(); // copy all data
   vert.isVert = contourVertices[0].isVert;
   vert.moveTo = false;
